038f2b5af7b0d6e2adf7de0d8c0dbd0959639e1d
Matilda Mared <matildamared@live.se>
2021-12-29 10:24:28 +0100

    complete exercise 5.6 & 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+	const [showDetails, setShowDetails] = useState(false);
+
+	const toggleShowDetails = () => {
+		setShowDetails(!showDetails);
+	};
+
+	return (
+		<>
+			{showDetails ? (
+				<div className="blog">
+					<p>
+						{blog.title} {blog.author}{" "}
+					</p>
+					<p>{blog.url}</p>
+					<p>Likes: {blog.likes}</p>
+					<p>{blog.user.name}</p>
+					<button onClick={toggleShowDetails}>Hide</button>
+				</div>
+			) : (
+				<div className="blog">
+					{blog.title} {blog.author}{" "}
+					<button onClick={toggleShowDetails}>View</button>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default Blog;
